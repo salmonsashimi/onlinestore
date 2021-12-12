0463f927998498f633173914a7704e971a522533
ME <cheung.samuelyc@gmail.com>
2021-12-13 01:10:37 +0800

    changed color scheme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
     return (
         <div className='navbar'>
             <div className='container navbar__content'>
-                <Link href='/'><h1 className='navbar__logo'>OPAL</h1></Link>
+                <Link href='/'><h1 className='navbar__logo'>NAVY</h1></Link>
                 <SearchBar />
                 <CartIcon />
             </div>
