5ee1ed2c4ca12adb834965eaa44575561567a833
ME <cheung.samuelyc@gmail.com>
2021-12-13 17:03:26 +0800

    added indexMainBanner images

--- src/components/IndexMainBanner.js
+++ src/components/IndexMainBanner.js
@@ -1,7 +1,14 @@
 const IndexMainBanner = () => (
     <div className='indexMainBanner'>
-        hello
+
+        <img src='https://picsum.photos/id/1011/1200/600' />
+
+
+
+
     </div>
 )
+// <img src='https://picsum.photos/id/590/1200/600' />
+// <img src='https://picsum.photos/id/177/1200/600' />
 
 export default IndexMainBanner;
