52bf3a83b350f783d8697dd9fbc778c97c8659fa
ME <cheung.samuelyc@gmail.com>
2021-12-14 01:00:14 +0800

    added social media on SignInBar

--- src/components/SignInBar.js
+++ src/components/SignInBar.js
@@ -1,9 +1,16 @@
 const SignInBar = () => (
     <div className='signInBar'>
         <div className='container signInBar__content'>
-            <p className='signInBar__link'>TRACK MY ORDER</p>
-            <p className='signInBar__link'>SIGN UP</p>
-            <p className='signInBar__link'>LOG IN</p>
+            <div className='signInBar__left'>
+                <p className='signInBar__link'>FACEBOOK</p>
+                <p className='signInBar__link'>INSTAGRAM</p>
+                <p className='signInBar__link'>YOUTUBE</p>
+            </div>
+            <div className='signInBar__right'>
+                <p className='signInBar__link'>TRACK MY ORDER</p>
+                <p className='signInBar__link'>SIGN UP</p>
+                <p className='signInBar__link'>LOG IN</p>
+            </div>
         </div>
     </div>
 )
