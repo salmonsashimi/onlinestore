66a08376502596945a395288e84d7f013b1dcac9
ME <cheung.samuelyc@gmail.com>
2021-12-15 02:34:14 +0800

    added dropdown for headerCategories

--- src/components/HeaderCategoryButton.js
+++ src/components/HeaderCategoryButton.js
@@ -1,6 +1,11 @@
 const HeaderCategoryButton = (props) => (
     <div className='headerCategoryButton'>
         <p className='headerCategoryButton__button'>{props.category.name}</p>
+        <div className='headerCategoryButton__dropdown'>
+            {props.category.dropdown.map((item) => (
+                <a>{item}</a>
+            ))}
+        </div>
     </div>
 )
 
