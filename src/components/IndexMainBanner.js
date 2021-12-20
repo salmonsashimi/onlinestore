a4b24b92ea8314b183dbfa088187a9e907709c9e
ME <cheung.samuelyc@gmail.com>
2021-12-20 21:31:16 +0800

    added carousel elements

--- src/components/IndexMainBanner.js
+++ src/components/IndexMainBanner.js
@@ -1,9 +1,15 @@
 const IndexMainBanner = () => (
     <div className='indexMainBanner container'>
-        <img src='https://picsum.photos/id/1011/1200/600' />
+        <div className='carouselDisplay'>
+            <img src='https://picsum.photos/id/1011/1200/600' />
+        </div>
+        <div className='carouselDisplay'>
+            <img src='https://picsum.photos/id/590/1200/600' />
+        </div>
+        <a className='carouselPreviousButton'></a>
+        <a className='carouselNextButton'></a>
     </div>
 )
-// <img src='https://picsum.photos/id/590/1200/600' />
 // <img src='https://picsum.photos/id/177/1200/600' />
 
 export default IndexMainBanner;
