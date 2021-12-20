afd0d8a7834b7930079a3cb7a3397828f0ca6da3
ME <cheung.samuelyc@gmail.com>
2021-12-20 21:44:30 +0800

    added default props

--- src/components/IndexMainBanner.js
+++ src/components/IndexMainBanner.js
@@ -1,15 +1,40 @@
-const IndexMainBanner = () => (
-    <div className='indexMainBanner container'>
-        <div className='carouselDisplay'>
-            <img src='https://picsum.photos/id/1011/1200/600' />
-        </div>
-        <div className='carouselDisplay'>
-            <img src='https://picsum.photos/id/590/1200/600' />
-        </div>
-        <a className='carouselPreviousButton'></a>
-        <a className='carouselNextButton'></a>
-    </div>
-)
-// <img src='https://picsum.photos/id/177/1200/600' />
+const IndexMainBanner = (props) => {
 
+
+
+
+
+    return (
+        <div className='indexMainBanner container'>
+            <div className='carousel'>
+
+                {props.banners.map(banner => (
+                    <div className='carouselDisplay'>
+                        <img src={banner.imgSrc} />
+                    </div>
+                ))}
+
+                <a className='carouselPreviousButton'></a>
+                <a className='carouselNextButton'></a>
+            </div>
+        </div>
+    )
+}
+
+
+IndexMainBanner.defaultProps = {
+    banners:
+        [
+            {
+                imgSrc: 'https://picsum.photos/id/1011/1200/600'
+            },
+            {
+                imgSrc: 'https://picsum.photos/id/590/1200/600'
+
+            },
+            {
+                imgSrc: 'https://picsum.photos/id/177/1200/600'
+
+            }]
+}
 export default IndexMainBanner;
