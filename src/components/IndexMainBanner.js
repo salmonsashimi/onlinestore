aea37d77f11dedd37a7c6ce429f124db9788fbc0
ME <cheung.samuelyc@gmail.com>
2021-12-20 22:23:10 +0800

    convert to class component

--- src/components/IndexMainBanner.js
+++ src/components/IndexMainBanner.js
@@ -1,25 +1,78 @@
-const IndexMainBanner = (props) => {
+import React, { useState } from 'react';
+class IndexMainBanner extends React.Component {
+    constructor(props) {
+        super(props)
+
+    }
 
 
-
-
-
-    return (
-        <div className='indexMainBanner container'>
-            <div className='carousel'>
-
-                {props.banners.map(banner => (
-                    <div className='carouselDisplay'>
-                        <img src={banner.imgSrc} />
-                    </div>
-                ))}
-
-                <a className='carouselPreviousButton'></a>
-                <a className='carouselNextButton'></a>
+    render() {
+        return (
+            <div className='indexMainBanner container'>
+                <div className='carousel'>
+                    {this.props.banners.map(banner => (
+                        <div className='carouselDisplay'>
+                            <img src={banner.imgSrc} />
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+// const IndexMainBanner = (props) => {
+
+//     const [banner, setBanner] = useState(0);
+
+
+//     const rotateCarousel = () => {
+//         console.log('hello')
+//         const totalBanners = props.banners;
+//         // const slides = document.getElementsByClassName('carouselDisplay')
+//         console.log(slides);
+
+//         for (let i = 0; i < totalBanners.length; i++) {
+//             console.log('thisistotalbanners', totalBanners);
+//         }
+
+//     };
+
+
+//     rotateCarousel();
+
+//     // var slideIndex = 0;
+//     // showSlides();
+
+//     // function showSlides() {
+//     //     var i;
+//     //     var slides = document.getElementsByClassName("carouselDisplay");
+//     //     for (i = 0; i < slides.length; i++) {
+//     //         slides[i].style.display = "none";
+//     //     }
+//     //     slideIndex++;
+//     //     if (slideIndex > slides.length) { slideIndex = 1 }
+//     //     slides[slideIndex - 1].style.display = "block";
+//     //     setTimeout(showSlides, 2000); // Change image every 2 seconds
+//     // }
+
+
+
+//     // return (
+//     //     <div className='indexMainBanner container'>
+//     //         <div className='carousel'>
+
+//     //             {props.banners.map(banner => (
+//     //                 <div className='carouselDisplay'>
+//     //                     <img src={banner.imgSrc} />
+//     //                 </div>
+//     //             ))}
+
+//     //             <a className='carouselPreviousButton'>prev</a>
+//     //             <a className='carouselNextButton'>next</a>
+//     //         </div>
+//     //     </div>
+//     // )
+// }
 
 
 IndexMainBanner.defaultProps = {
