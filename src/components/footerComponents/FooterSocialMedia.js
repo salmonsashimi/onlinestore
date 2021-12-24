6901cf53910ba48f48a1cf812af4c31d55c07697
ME <cheung.samuelyc@gmail.com>
2021-12-25 07:23:01 +0800

    added email sub form

--- src/components/footerComponents/FooterSocialMedia.js
+++ src/components/footerComponents/FooterSocialMedia.js
@@ -1,6 +1,12 @@
 const FooterSocialMedia = () => (
     <div>
         <p>Download mobile app here</p>
+
+        <h3 className='footerSection__name'>Subscribe to our mailing list</h3>
+        <form>
+            <input type='text'></input>
+            <button>Subscribe</button>
+        </form>
     </div>
 )
 
