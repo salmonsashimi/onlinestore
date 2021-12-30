b3d94549c7ed78303a6c4f64a3782086db83e47b
ME <cheung.samuelyc@gmail.com>
2021-12-30 20:12:25 +0800

    adjusted font weight

--- src/components/headerComponents/HeaderCategoryButton.js
+++ src/components/headerComponents/HeaderCategoryButton.js
@@ -3,7 +3,7 @@ const HeaderCategoryButton = (props) => (
         <p className='headerCategoryButton__button'>{props.category.name}</p>
         <div className='headerCategoryButton__dropdown'>
             {props.category.dropdown.map((item) => (
-                <a key={item}>{item}</a>
+                <a className='headerCategoryButton__item' key={item}>{item}</a>
             ))}
         </div>
     </div>
