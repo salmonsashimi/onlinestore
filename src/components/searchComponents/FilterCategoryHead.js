677365e67e6a1567eab4dac778491218b50f697f
ME <cheung.samuelyc@gmail.com>
2022-01-01 14:13:22 +0800

    added hooks to toggle collapisible

--- src/components/searchComponents/FilterCategoryHead.js
+++ src/components/searchComponents/FilterCategoryHead.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
+
 const FilterCategoryHead = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div className='filterCategoryHead'>
-            <button className='filterCategoryHead__button'>open</button>
-            <div className='filterCategoryHead__content'>
+            <button className='filterCategoryHead__button' onClick={() => setIsOpen(!isOpen)}>open</button>
+            <div className={
+                `filterCategoryHead__content 
+                ${isOpen ? 'filterCategoryHead__contentShow' : null}
+                `
+            } >
                 stuff
         </div>
-        </div>
+        </div >
     )
 }
 
