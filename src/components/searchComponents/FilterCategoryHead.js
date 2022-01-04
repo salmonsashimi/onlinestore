84906c9a704a5aff304e636cac00be9791b8dd8e
ME <cheung.samuelyc@gmail.com>
2022-01-04 22:13:01 +0800

    added categories

--- src/components/searchComponents/FilterCategoryHead.js
+++ src/components/searchComponents/FilterCategoryHead.js
@@ -6,11 +6,13 @@ const FilterCategoryHead = (props) => {
 
     return (
         <div className='filterCategoryHead'>
-            <button className='filterCategoryHead__button' onClick={() => setIsOpen(!isOpen)}>open</button>
+            <button className='filterCategoryHead__button' onClick={() => setIsOpen(!isOpen)}>{props.category.name}</button>
             <div className={
                 `filterCategoryHead__content ${isOpen ? 'filterCategoryHead__contentShow' : null}`
             } >
-                {props.categories}
+                {props.category.dropdown.map(item => (
+                    <p>{item}</p>
+                ))}
             </div>
         </div>
     )
