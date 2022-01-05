01b68e911f92c9b126cc17b20f2edeca63c7b576
ME <cheung.samuelyc@gmail.com>
2022-01-05 21:21:59 +0800

    adjusted grid size

--- src/components/searchComponents/ItemBoxes.js
+++ src/components/searchComponents/ItemBoxes.js
@@ -1,12 +1,18 @@
 import ItemBox from './ItemBox';
 
-const ItemBoxes = () => (
-    <div className='itemBoxes'>
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-    </div>
-)
+const ItemBoxes = () => {
+    return (
+        <div className='itemBoxes'>
+            <ItemBox />
+            <ItemBox />
+            <ItemBox />
+            <ItemBox />
+            <ItemBox />
+            <ItemBox />
+            <ItemBox />
+            <ItemBox />
+        </div>
+    )
+}
 
 export default ItemBoxes;
