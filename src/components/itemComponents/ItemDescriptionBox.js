4b5032aabd02c8d7ad8ac196f1fdc7b0f60d7e1a
ME <cheung.samuelyc@gmail.com>
2022-01-07 00:43:13 +0800

    added elements

--- src/components/itemComponents/ItemDescriptionBox.js
+++ src/components/itemComponents/ItemDescriptionBox.js
@@ -1,6 +1,11 @@
 const ItemDescriptionBox = () => (
     <div>
-        hello
+        <h3>Nike</h3>
+        <h3>Brand new sneakers</h3>
+        <h3>$100.00</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate mi sit amet mauris commodo quis imperdiet. Nisi lacus sed viverra tellus in hac habitasse.</p>
+        <button>ADD TO CART</button>
+
     </div>
 )
 
