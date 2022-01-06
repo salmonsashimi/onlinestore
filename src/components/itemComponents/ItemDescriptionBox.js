994a2fb02aa4ef6076ed28245c407ba06eb6b693
ME <cheung.samuelyc@gmail.com>
2022-01-07 00:45:43 +0800

    added classes

--- src/components/itemComponents/ItemDescriptionBox.js
+++ src/components/itemComponents/ItemDescriptionBox.js
@@ -1,10 +1,10 @@
 const ItemDescriptionBox = () => (
-    <div>
-        <h3>Nike</h3>
-        <h3>Brand new sneakers</h3>
-        <h3>$100.00</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate mi sit amet mauris commodo quis imperdiet. Nisi lacus sed viverra tellus in hac habitasse.</p>
-        <button>ADD TO CART</button>
+    <div className='itemDescriptionBox'>
+        <h3 className='itemDescriptionBox__brand'>Nike</h3>
+        <h3 className='itemDescriptionBox__itemName'>Brand new sneakers</h3>
+        <h3 className='itemDescriptionBox__price'>$100.00</h3>
+        <p className='itemDescriptionBox__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate mi sit amet mauris commodo quis imperdiet. Nisi lacus sed viverra tellus in hac habitasse.</p>
+        <button className='itemDescriptionBox__button'>ADD TO CART</button>
 
     </div>
 )
