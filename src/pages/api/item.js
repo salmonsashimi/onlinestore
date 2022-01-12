457aefda9bac52812dbd5581be0236254856f17f
ME <cheung.samuelyc@gmail.com>
2022-01-13 00:25:01 +0800

    linked item api to item page

--- src/pages/api/item.js
+++ src/pages/api/item.js
@@ -1,16 +1,35 @@
-import { connectToDatabase } from "../../util/mongodb";
+// import { connectToDatabase } from "../../util/mongodb";
 
-export default async (req, res) => {
-    const { db } = await connectToDatabase();
-    const movies = await db
-        .collection("movies")
-        .find({})
-        .sort({ metacritic: -1 })
-        .limit(20)
-        .toArray();
-    res.json(movies);
-};
+// export default async (req, res) => {
+//     const { db } = await connectToDatabase();
+//     const movies = await db
+//         .collection("movies")
+//         .find({})
+//         .sort({ metacritic: -1 })
+//         .limit(20)
+//         .toArray();
+//     res.json(movies);
+// };
 
+
+import clientPromise from '../../lib/mongodb';
+
+export default async function (req, res) {
+
+    const client = await clientPromise;
+    const db = client.db('online-store');
+    const col = db.collection("items");
+
+
+    if (req.method === 'POST') {
+        // Process a POST request
+    } else {
+        // Handle any other HTTP method
+        const item = await col.findOne({ "id": 1 });
+        res.json(item)
+
+    }
+}
 
 // export async function getServerSideProps(context) {
 //     try {
