3069611a5504fa63a4d64bcce103a7677eabd2e4
ME <cheung.samuelyc@gmail.com>
2022-01-13 13:38:16 +0800

    connect search api to itemBox

--- src/components/searchComponents/ItemBox.js
+++ src/components/searchComponents/ItemBox.js
@@ -1,14 +1,14 @@
 const ItemBox = (props) => {
     const { name, brand, price, imgUrl } = props.item;
-    console.log(name)
 
     return (
         <div className='itemBox'>
-            <img src={imgUrl} />
+            <img className='itemBox__img' src={imgUrl} />
             <p className='itemBox__brand'>{brand}</p>
             <p className='itemBox__name'>{name}</p>
             <p className='itemBox__price'>${price}</p>
         </div>
     )
 }
+
 export default ItemBox;
