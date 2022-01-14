86fa5c42604970a93a5edbd09e3145fec3a68af7
ME <cheung.samuelyc@gmail.com>
2022-01-15 06:56:21 +0800

    linked individual ItemBox with item route

--- src/components/searchComponents/ItemBox.js
+++ src/components/searchComponents/ItemBox.js
@@ -1,8 +1,18 @@
+import { useRouter } from 'next/router';
+
 const ItemBox = (props) => {
-    const { name, brand, price, imgUrl } = props.item;
+    const { id, name, brand, price, imgUrl } = props.item;
+
+    const router = useRouter();
+
+    const itemClick = () => {
+        console.log(id)
+        router.push(`/item/${id}`)
+
+    }
 
     return (
-        <div className='itemBox'>
+        <div className='itemBox' onClick={itemClick}>
             <img className='itemBox__img' src={imgUrl} />
             <p className='itemBox__brand'>{brand}</p>
             <p className='itemBox__name'>{name}</p>
