776020327f531c92bf6831dff8ca31f5a24590b3
ME <cheung.samuelyc@gmail.com>
2022-01-16 18:57:01 +0800

    added infoBar

---
+++ src/components/indexComponents/InfoBar.js
@@ -0,0 +1,8 @@
+const InfoBar = () => (
+    <div className='infoBar'>
+        <h3 className='infoBar__text'>EASY DELIVERY WORDLWIDE</h3>
+        <h3 className='infoBar__text'>DISCOVER OVER 500 BRANDS</h3>
+    </div>
+)
+
+export default InfoBar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import IndexCarousel from '../components/indexComponents/IndexCarousel';
 import IndexPromosBanner from '../components/indexComponents/IndexPromosBanner';
 import IndexCategories from '../components/indexComponents/IndexCategories';
+import InfoBar from '../components/indexComponents/InfoBar';
 
 const Index = () => (
     <>
         <IndexCarousel />
         <IndexPromosBanner />
         <IndexCategories />
+        <InfoBar />
     </>
 );
 
