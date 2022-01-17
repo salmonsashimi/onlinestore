80e29feb51bf263dcd3c5f0106c7b1bcfaaea127
ME <cheung.samuelyc@gmail.com>
2022-01-17 08:01:43 +0800

    added IndexSustainabilityBanner

---
+++ src/components/indexComponents/IndexSustainabilityBanner.js
@@ -0,0 +1,14 @@
+const IndexSustainabilityBanner = () => (
+    <div className='container indexSustainabilityBanner'>
+        <div className='indexSustainabilityBanner__left'>
+            left side
+    
+        </div>
+        <div className='indexSustainabilityBanner__right'>
+            right side
+        </div>
+        
+    </div>
+)
+
+export default IndexSustainabilityBanner;
