8fa0702297dd39a966a1b7ea84eee17895187168
ME <cheung.samuelyc@gmail.com>
2022-01-17 18:43:50 +0800

    changed info content

--- src/components/indexComponents/InfoBar.js
+++ src/components/indexComponents/InfoBar.js
@@ -1,7 +1,7 @@
 const InfoBar = () => (
     <div className='infoBar'>
-        <h3 className='infoBar__text'>EASY DELIVERY WORDLWIDE</h3>
         <h3 className='infoBar__text'>DISCOVER OVER 500 BRANDS</h3>
+        <h3 className='infoBar__text'>EASY DELIVERY WORDLWIDE</h3>
     </div>
 )
 
