e5049804ae57d62ca80768d0c5d474727098ea1a
ME <cheung.samuelyc@gmail.com>
2022-01-18 20:23:35 +0800

    added img

--- src/components/indexComponents/IndexTrendingBanner.js
+++ src/components/indexComponents/IndexTrendingBanner.js
@@ -1,13 +1,17 @@
 const IndexTrendingBanner = () => (
     <div className='container indexTrendingBanner'>
         <div className='indexTrendingBanner__left'>
-            <h3>THREAD</h3>
+            <img src='https://picsum.photos/id/116/450/600'></img>
+            <h3>Floral</h3>
             <p>by NAVY</p>
+            <button>shop now</button>
 
         </div>
         <div className='indexTrendingBanner__right'>
-            <p>Sustainability fashion</p>
+            <img src='https://picsum.photos/id/106/450/600'></img>
+            <h3>Sustainability fashion</h3>
             <p>Explore clothing made from recyclable materials</p>
+            <button>shop now</button>
         </div>
 
     </div>
