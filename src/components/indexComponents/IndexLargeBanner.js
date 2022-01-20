4084eb648f57006a7c8ef2aa7c121069690b48d9
ME <cheung.samuelyc@gmail.com>
2022-01-20 17:44:02 +0800

    added IndexLargeBanner

---
+++ src/components/indexComponents/IndexLargeBanner.js
@@ -0,0 +1,11 @@
+const IndexLargeBanner = () => (
+    <div className='container'>
+        <div className='indexLargeBanner'>
+            <h3 className='indexLargeBanner__title'>Chinese New Year Sale!</h3>
+            <p className='indexLargeBanner__text'>Red hot sale at 70% off!</p>
+            <button className='indexLargeBanner__button'>SHOP NOW</button>
+
+        </div>
+    </div>
+)
+export default IndexLargeBanner;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import IndexCarousel from '../components/indexComponents/IndexCarousel';
 import IndexPromosBanner from '../components/indexComponents/IndexPromosBanner';
 import IndexCategories from '../components/indexComponents/IndexCategories';
 import IndexTrendingBanner from '../components/indexComponents/IndexTrendingBanner';
+import IndexLargeBanner from '../components/indexComponents/IndexLargeBanner';
 import InfoBar from '../components/indexComponents/InfoBar';
 
 
@@ -9,6 +10,7 @@ const Index = () => (
     <>
         <IndexCarousel />
         <IndexPromosBanner />
+        <IndexLargeBanner />
         <IndexTrendingBanner />
         <IndexCategories />
         <InfoBar />
