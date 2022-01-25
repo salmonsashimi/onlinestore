eaeb7e76a5d9dfd7c24fa6f7054310167b04eb16
ME <cheung.samuelyc@gmail.com>
2022-01-25 15:33:33 +0800

    styled cart

--- src/components/headerComponents/CartIcon.js
+++ src/components/headerComponents/CartIcon.js
@@ -16,7 +16,7 @@ class CartIcon extends Component {
         return (
             <div className='cartIcon'>
                 <Link href='/cart'><FaShoppingCart size='1.5em' className='cartIcon__button' /></Link>
-                <div className='cartIcon__dropdown'>
+                <div className='cartIcon__dropdown cartIcon__border'>
                     <h3 className='cartIcon__dropdown-header'>My Cart, <span className='cartItem__dropdown-itemNo'>{cartItems.length} item(s)</span></h3>
                     {cartItems.length !== 0 ?
                         cartItems.map(item => (
