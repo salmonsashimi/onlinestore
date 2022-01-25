15d6f2d87ad201e8e2c8c44d84adac80bf941690
ME <cheung.samuelyc@gmail.com>
2022-01-25 08:02:02 +0800

    added cart buttons

--- src/components/headerComponents/CartIcon.js
+++ src/components/headerComponents/CartIcon.js
@@ -17,7 +17,7 @@ class CartIcon extends Component {
             <div className='cartIcon'>
                 <Link href='/cart'><FaShoppingCart size='1.5em' className='cartIcon__button' /></Link>
                 <div className='cartIcon__dropdown'>
-                    <h3 className='cartItem__dropdown-header'>My Cart, <span className='cartItem__dropdown-itemNo'>{cartItems.length} item(s)</span></h3>
+                    <h3 className='cartIcon__dropdown-header'>My Cart, <span className='cartItem__dropdown-itemNo'>{cartItems.length} item(s)</span></h3>
                     {cartItems.length !== 0 ?
                         cartItems.map(item => (
                             <a href='#'> {item}</a>
@@ -25,7 +25,14 @@ class CartIcon extends Component {
                         :
                         <a>Cart is empty</a>
                     }
-
+                    <div className='cartIcon__priceSection'>
+                        <h3 className='cartIcon__dropdown-subtotal'>Subtotal:</h3>
+                        <h3 className='cartIcon__dropdown-price'>$00.00</h3>
+                    </div>
+                    <div className='cartIcon__dropdown-buttons'>
+                        <button className='cartIcon__dropdown-button'>VIEW CART</button>
+                        <button className='cartIcon__dropdown-button'>CHECKOUT</button>
+                    </div>
                 </div>
             </div>
         )
