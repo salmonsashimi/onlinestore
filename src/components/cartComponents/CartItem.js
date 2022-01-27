e99cdd2c111f1c15e585df18c6defb1b62ba6e76
ME <cheung.samuelyc@gmail.com>
2022-01-28 07:14:02 +0800

    styling for cartItem

--- src/components/cartComponents/CartItem.js
+++ src/components/cartComponents/CartItem.js
@@ -1,6 +1,13 @@
 const CartItem = () => (
     <div className='cartItem'>
-        cartitem
+        <div>img</div>
+        <div className='cartItem__description'>
+            <p>brand</p>
+            <p>item</p>
+            <p>price</p>
+            <p>quantity</p>
+        </div>
+        <button className='cartItem__button'>X</button>
     </div>
 )
 
