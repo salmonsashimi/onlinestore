ab482d325d53c9cef8ea4bf937b41a60f8674204
ME <cheung.samuelyc@gmail.com>
2022-01-30 18:57:40 +0800

    added context

--- src/components/cartComponents/CartItems.js
+++ src/components/cartComponents/CartItems.js
@@ -1,9 +1,32 @@
+import { Component } from 'react';
+import { CartContext } from '../../contexts/CartContext';
 import CartItem from './CartItem';
 
-const CartItems = () => (
-    <div>
-        <CartItem />
-    </div>
-)
+class CartItems extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+
+    render() {
+        let items = this.context.cart;
+
+        return (
+            <div>
+                {items.length ?
+                    items.map(item => (
+                        <CartItem item={item} />
+                    ))
+
+                    :
+                    <p>You have no items</p>
+                }
+            </div>
+        )
+    }
+}
+
+
+CartItems.contextType = CartContext;
 
 export default CartItems;
