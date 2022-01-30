9446e42a0b323c3d31b9b70dc19c3725952c78cc
ME <cheung.samuelyc@gmail.com>
2022-01-31 07:08:03 +0800

    styled CartItem

--- src/components/cartComponents/CartItem.js
+++ src/components/cartComponents/CartItem.js
@@ -1,11 +1,10 @@
 const CartItem = (props) => (
     <div className='cartItem'>
-        <img src={props.item.imgUrl} />
+        <img src={props.item.imgUrl} className='cartItem__img' />
         <div className='cartItem__description'>
-            <p>{props.item.brand}</p>
-            <p>{props.item.name}</p>
-            <p>{props.item.price}</p>
-            <p>{props.item.quantity}</p>
+            <p><span className='cartItem__description-brand'>{props.item.brand}</span> {props.item.name}</p>
+            <p className='cartItem__description-price'>SGD{props.item.price}</p>
+            <p>Qty: {props.item.quantity}</p>
         </div>
         <button className='cartItem__button'>X</button>
     </div>
