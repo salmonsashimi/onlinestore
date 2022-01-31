9c5b931b06d3eeebc2ecbd2c9d9dd014d50ad4d0
ME <cheung.samuelyc@gmail.com>
2022-02-01 07:05:17 +0800

    added styles for cartSummary

--- src/components/cartComponents/CartSummary.js
+++ src/components/cartComponents/CartSummary.js
@@ -1,14 +1,18 @@
 
 const CartSummary = () => (
-    <form>
-        <h1>Total</h1>
-        <label for='delivery'>Delivery</label>
-        <select name='delivery' id='delivery' >
-            <option value='standard'>Standard Delivery (Free)</option>
-            <option value='express'>Express Delivery ($10.00)</option>
-        </select>
-        <label for='discount'>Discount Code: </label>
-        <input name='discount' id='discount' type='text'></input>
+    <form className='cartSummary'>
+        <h1 className='cartSummary__header'>Total</h1>
+        <div>
+            <label for='delivery'>Delivery</label>
+            <select name='delivery' id='delivery' >
+                <option value='standard'>Standard Delivery (Free)</option>
+                <option value='express'>Express Delivery ($10.00)</option>
+            </select>
+        </div>
+        <div>
+            <label for='discount'>Discount Code: </label>
+            <input name='discount' id='discount' type='text'></input>
+        </div>
         <h3>Sub-total</h3>
         <button>CHECKOUT</button>
     </form>
