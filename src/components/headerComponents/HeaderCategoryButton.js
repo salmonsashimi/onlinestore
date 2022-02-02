0917990a9dc373da001cec17b23bb525508f5319
ME <cheung.samuelyc@gmail.com>
2022-02-03 02:38:56 +0800

    adjusted button hover styles

--- src/components/headerComponents/HeaderCategoryButton.js
+++ src/components/headerComponents/HeaderCategoryButton.js
@@ -1,12 +1,21 @@
-const HeaderCategoryButton = (props) => (
-    <div className='headerCategoryButton'>
-        <p className='headerCategoryButton__button'>{props.category.name}</p>
-        <div className='headerCategoryButton__dropdown'>
-            {props.category.dropdown.map((item) => (
-                <a className='headerCategoryButton__item' key={item}>{item}</a>
-            ))}
+import { useState } from 'react';
+
+const HeaderCategoryButton = (props) => {
+
+    const [over, setOver] = useState();
+
+
+
+    return (
+        <div className='headerCategoryButton'>
+            <p className={`headerCategoryButton__button ${over}`} onMouseOver={() => setOver('active')} onMouseOut={() => setOver(null)}>{props.category.name}</p>
+            <div className='headerCategoryButton__dropdown' onMouseOver={() => setOver('active')} onMouseOut={() => setOver(null)}>
+                {props.category.dropdown.map((item) => (
+                    <a className='headerCategoryButton__item' key={item}>{item}</a>
+                ))}
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default HeaderCategoryButton;
