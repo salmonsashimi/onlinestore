6dd1da1474e500370a4ee17747a46856437faa16
ME <cheung.samuelyc@gmail.com>
2022-02-04 14:37:22 +0800

    added styles for cartDropdownItem

--- src/components/headerComponents/CartDropdownItem.js
+++ src/components/headerComponents/CartDropdownItem.js
@@ -1,13 +1,21 @@
-const CartDropdownItem = (props) => (
-    <div className='cartDropdownItem'>
-        <img src={props.item.imgUrl} className='cartDropdownItem__img' />
-        <div className='cartDropdownItem__desc'>
-            <p><span className='cartDropdownItem__desc-brand'>{props.item.brand}</span> {props.item.name}</p>
-            <p className='cartDropdownItem__desc-price'>SGD${props.item.price}</p>
-            <p>Qty: {props.item.quantity}</p>
-        </div>
-    </div>
+import Link from 'next/link';
 
-)
+const CartDropdownItem = (props) => {
+
+    const { id, imgUrl, name, brand, price, quantity } = props.item;
+    return (
+        <Link href={`/item/${id}`}>
+            <div className='cartDropdownItem'>
+                <img src={imgUrl} className='cartDropdownItem__img' />
+                <div className='cartDropdownItem__desc'>
+                    <p className='cartDropdownItem__desc-name'><span className='cartDropdownItem__desc-brand'>{brand}</span> {name}</p>
+                    <p className='cartDropdownItem__desc-price'>SGD${price}</p>
+                    <p>Qty: {quantity}</p>
+                </div>
+            </div>
+        </Link>
+
+    )
+}
 
 export default CartDropdownItem;
