43d9691f8d3a590575ffa7f314681ee04e63de25
ME <cheung.samuelyc@gmail.com>
2022-02-05 15:29:33 +0800

    added scroll bar to cartIcon

--- src/components/headerComponents/CartIcon.js
+++ src/components/headerComponents/CartIcon.js
@@ -18,16 +18,18 @@ class CartIcon extends Component {
                 <Link href='/cart'><FaShoppingCart size='1.5em' className='cartIcon__button' /></Link>
                 <div className='cartIcon__dropdown cartIcon__border'>
                     <h3 className='cartIcon__dropdown-header'>My Cart, <span className='cartItem__dropdown-itemNo'>{cartItems.length} item(s)</span></h3>
-                    {cartItems.length !== 0 ?
-                        cartItems.map(item => {
-                            totalPrice += item.price;
-                            return (
-                                <CartDropdownItem item={item} />
-                            )
-                        })
-                        :
-                        <p>Cart is empty</p>
-                    }
+                    <div className='cartIcon__cartDropdownItems'>
+                        {cartItems.length !== 0 ?
+                            cartItems.map(item => {
+                                totalPrice += item.price;
+                                return (
+                                    <CartDropdownItem item={item} />
+                                )
+                            })
+                            :
+                            <p>Cart is empty</p>
+                        }
+                    </div>
                     <div className='cartIcon__priceSection'>
                         <h3 className='cartIcon__dropdown-subtotal'>Subtotal:</h3>
                         <h3 className='cartIcon__dropdown-price'>${totalPrice}</h3>
