23f1e964d29e9d95acb389bf1410c72e0713fa91
ME <cheung.samuelyc@gmail.com>
2022-02-08 06:52:04 +0800

    converted class based to fucntional component

--- src/components/cartComponents/CartItems.js
+++ src/components/cartComponents/CartItems.js
@@ -1,30 +1,21 @@
-import { Component } from 'react';
+import { useContext } from 'react';
 import { CartContext } from '../../contexts/CartContext';
 import CartItem from './CartItem';
 
-class CartItems extends Component {
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        let items = this.context.cart;
-        let removeItem = this.context.removeItem;
-
-        return (
-            <div className='cartItems'>
-                {
-                    items.length ?
-                        items.map(item => <CartItem item={item} removeItem={removeItem} />)
-                        :
-                        <p>You have no items</p>
-                }
-            </div>
-        )
-    }
+const CartItems = () => {
+    const cartContext = useContext(CartContext);
+    const items = cartContext.cart;
+    const removeItem = cartContext.removeItem;
+    return (
+        <div className='cartItems'>
+            {
+                items.length ?
+                    items.map(item => <CartItem item={item} removeItem={removeItem} />)
+                    :
+                    <p>You have no items</p>
+            }
+        </div>
+    )
 }
-
-
-CartItems.contextType = CartContext;
 
 export default CartItems;
