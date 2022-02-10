3974bebabcd627af5d18b0f96e1a64fb01d4b60e
ME <cheung.samuelyc@gmail.com>
2022-02-10 18:19:09 +0800

    styled cartSummary

--- src/components/cartComponents/CartSummary.js
+++ src/components/cartComponents/CartSummary.js
@@ -8,19 +8,18 @@ const CartSummary = () => {
     return (
         <form className='cartSummary'>
             <h1 className='cartSummary__header'>Total</h1>
-            <div className='cartSummary__promoCode'>
-                <h3 className='cartSummary__promoCode-label'>PROMO CODE: </h3>
-                <input className='cartSummary__promoCode-input' name='discount' id='discount' type='text' placeholder='Enter Promo Code'></input>
-                <button className='cartSummary__promoCode-button' >APPLY</button>
+            <div className='cartSummary__subtotal'>
+                <h3 className='cartSummary__subtotal-title'>Sub-total</h3>
+                <h3 className='cartSummary__subtotal-price'>SGD$10000.00</h3>
             </div>
             <div className='cartSummary__delivery'>
-                <label className='cartSummary__delivery-label' for='delivery'>DELIVERY:</label>
+                <h3 className='cartSummary__delivery-title'>Delivery</h3>
                 <select className='cartSummary__delivery-options' name='delivery' id='delivery' >
-                    <option value='standard'>Standard Delivery (Free)</option>
-                    <option value='express'>Express Delivery ($10.00)</option>
+                    <option value='standard'>Standard (Free)</option>
+                    <option value='express'>Express ($50.00)</option>
                 </select>
             </div>
-            <h3>Sub-total</h3>
+
             <button className='cartSummary__button'>CHECKOUT</button>
         </form>
     )
