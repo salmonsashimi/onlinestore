033075c871f7213d4c0f2b7dff13de0db0ef799f
ME <cheung.samuelyc@gmail.com>
2022-02-10 18:50:46 +0800

    adjusted alignment of checkbox and label

--- src/components/searchComponents/FilterCategoryItem.js
+++ src/components/searchComponents/FilterCategoryItem.js
@@ -1,6 +1,6 @@
 const FilterCategoryItem = (props) => (
     <div className='filterCategoryItem'>
-        <input type='checkbox' name={props.item} value={props.item} />
+        <input className='filterCateogryItem__checkbox' type='checkbox' name={props.item} value={props.item} />
         <label className='filterCateogryItem__label'>{props.item}</label>
     </div>
 )
