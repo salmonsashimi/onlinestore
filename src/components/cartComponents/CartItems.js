1d378d2de80c5238d1dcc4e7978cc72f1a05edce
ME <cheung.samuelyc@gmail.com>
2022-02-13 07:56:08 +0800

    added header for CartItems

--- src/components/cartComponents/CartItems.js
+++ src/components/cartComponents/CartItems.js
@@ -8,12 +8,15 @@ const CartItems = () => {
     const removeItem = cartContext.removeItem;
     return (
         <div className='cartItems'>
-            {
-                items.length ?
-                    items.map(item => <CartItem item={item} removeItem={removeItem} />)
-                    :
-                    <p>You have no items</p>
-            }
+            <h3 className='cartItems__header'>MY CART</h3>
+            <div>
+                {
+                    items.length ?
+                        items.map(item => <CartItem item={item} removeItem={removeItem} />)
+                        :
+                        <p>You have no items</p>
+                }
+            </div>
         </div>
     )
 }
