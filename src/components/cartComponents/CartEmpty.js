cd843556b69aaa29fad20b67742980bcb5ba20c1
ME <cheung.samuelyc@gmail.com>
2022-02-16 06:56:18 +0800

    styled cartEmpty

--- src/components/cartComponents/CartEmpty.js
+++ src/components/cartComponents/CartEmpty.js
@@ -1,6 +1,11 @@
+import Link from 'next/link';
+
 const CartEmpty = () => (
-    <div>
-        no items
+    <div className='cartEmpty'>
+        <h3 className='cartEmpty__header'>YOUR CART IS EMPTY</h3>
+        <Link href='/'>
+            <a className='cartEmpty__button'>SHOP NOW</a>
+        </Link>
     </div>
 )
 
