677f9e1c4143256d6622b42c7369242b8ab577f9
ME <cheung.samuelyc@gmail.com>
2022-02-15 20:03:15 +0800

    adjusted label and styling

--- src/components/searchComponents/FilterCategoryItem.js
+++ src/components/searchComponents/FilterCategoryItem.js
@@ -1,7 +1,9 @@
 const FilterCategoryItem = (props) => (
     <div className='filterCategoryItem'>
-        <input className='filterCateogryItem__checkbox' type='checkbox' name={props.item} value={props.item} />
-        <label className='filterCateogryItem__label'>{props.item}</label>
+        <label className='filterCateogryItem__label'>
+            <input className='filterCateogryItem__checkbox' type='checkbox' name={props.item} value={props.item} />
+            {props.item}
+        </label>
     </div>
 )
 
