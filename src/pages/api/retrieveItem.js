943d9d0b72137a8d3ec2caa92f09a212ba757a3b
ME <cheung.samuelyc@gmail.com>
2022-02-18 02:17:56 +0800

    configure mongo to find all items

--- src/pages/api/retrieveItem.js
+++ src/pages/api/retrieveItem.js
@@ -4,36 +4,24 @@ import { findUser } from './user'
 
 export default async () => {
     const client = await clientPromise;
-    const user = await findUser();
+    // const user = await findUser();
 
 
 
     const db = client.db('online-store');
-    const itemCol = db.collection("items");
+    const col = await db
+        .collection("items")
+        .find({}, { name: 1, brand: 1 })
+        .toArray();
 
 
-    // Construct a document            
+    console.log(col)
 
-
-    let itemDocument = {
-        "id": 1,
-        "user": user,
-        "name": "Denim Jacket",
-        "brand": "Adidas",
-        "price": 2340.21,
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nulla aliquet porttitor lacus luctus accumsan tortor.",
-        "imgUrl": "https://picsum.photos/id/240/400/550"
-    }
 
 
 
     // Insert a single document, wait for promise so we can read it back
-    const p = await itemCol.insertOne(itemDocument);
-
-
-
-
-
+    // const p = await itemCol.insertOne(itemDocument);
 
 
 
