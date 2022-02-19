19549e404dd0a117f202f431615ab53a61f7f3a9
ME <cheung.samuelyc@gmail.com>
2022-02-19 16:29:19 +0800

    adjusted search query results

--- src/pages/api/search/[pid].js
+++ src/pages/api/search/[pid].js
@@ -3,11 +3,19 @@ import clientPromise from '../../../lib/mongodb';
 export default async function (req, res) {
     const data = await (await fetch(`http://localhost:3000/api/retrieveItems`)).json()
 
-    res.json(data)
-
+    // console.log('data', data);
 
     //retrieve search query
     const { pid } = req.query;
-    // console.log(pid);
+    console.log(pid);
+
+    const results = data.filter(item => {
+        return item.name.includes(pid) || item.brand.includes(pid)
+    })
+
+    console.log('results', results)
+
+    res.json(results)
+
 
 }
