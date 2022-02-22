6bf8c1cc34e2826c0ea6a5329b3bfdc3a43c46c9
ME <cheung.samuelyc@gmail.com>
2022-02-23 00:01:08 +0800

    adjusted retrieveItems api

--- src/components/headerComponents/NavBar.js
+++ src/components/headerComponents/NavBar.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Link from 'next/link';
 import SearchBar from './SearchBar';
 import CartIcon from './CartIcon';
@@ -5,19 +6,22 @@ import { useEffect } from 'react';
 
 const NavBar = (props) => {
 
+    const [results, setResults] = useState([]);
+
     useEffect(() => {
         fetch('http://localhost:3000/api/retrieveItems')
             .then((res) => res.json())
             .then((data) => {
                 console.log('data', data)
+                setResults(data);
             })
-    })
+    }, [])
 
     return (
         <div className='navbar'>
             <div className='container navbar__content'>
                 <Link href='/'><h1 className='navbar__logo'>NAVY</h1></Link>
-                <SearchBar results={props.data} />
+                <SearchBar results={results} />
                 <CartIcon />
             </div>
         </div>
