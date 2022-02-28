4c2265ed376e433b85bdd796497408ec734476da
ME <cheung.samuelyc@gmail.com>
2022-02-28 18:45:48 +0800

    added page title

--- src/components/headerComponents/Header.js
+++ src/components/headerComponents/Header.js
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import NavBar from './NavBar'
 import SignInBar from './SignInBar';
 import HeaderCategoriesBar from './HeaderCategoriesBar';
 
 const Header = () => (
     <div className='header'>
+        <Head>
+            <title>NAVY | Fashion & Lifestyle Shopping Online</title>
+        </Head>
         <SignInBar />
         <NavBar />
         <HeaderCategoriesBar />
