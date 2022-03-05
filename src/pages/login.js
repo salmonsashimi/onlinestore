339f8b419cdc6add14d9cada4ca53b783864c1d0
ME <cheung.samuelyc@gmail.com>
2022-03-05 18:48:21 +0800

    added div for background color

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,31 +1,32 @@
 const LoginPage = () => {
     return (
-
         <div className='login'>
-            <div className='login__links'>
-                <a className='login__links-login'>LOGIN</a>
-                <a className='login__links-signUp'>SIGN UP</a>
-            </div>
-            <div>
-                <h1 className='login__header'>WELCOME TO</h1>
-                <div className='login__logo'>
-                    <h1 className='login__logo-text'>NAVY</h1>
+            <div className='login__component'>
+                <div className='login__links'>
+                    <a className='login__links-login'>LOGIN</a>
+                    <a className='login__links-signUp'>SIGN UP</a>
                 </div>
-                <form className='login__form'>
-                    <div className='login__email'>
-                        <p className='login__email-header'>EMAIL ADDRESS:</p>
-                        <input type='text' className='login__email-input' />
+                <div>
+                    <h1 className='login__header'>WELCOME TO</h1>
+                    <div className='login__logo'>
+                        <h1 className='login__logo-text'>NAVY</h1>
                     </div>
-                    <div className='login__password'>
-                        <p className='login__password-header'>PASSWORD:</p>
-                        <input type='password' className='login__password-input' />
-                    </div>
-                    <button className='login__button'>SIGN IN</button>
-                    <p className='login__forgotPassword'>FORGOT PASSWORD?</p>
+                    <form className='login__form'>
+                        <div className='login__email'>
+                            <p className='login__email-header'>EMAIL ADDRESS:</p>
+                            <input type='text' className='login__email-input' />
+                        </div>
+                        <div className='login__password'>
+                            <p className='login__password-header'>PASSWORD:</p>
+                            <input type='password' className='login__password-input' />
+                        </div>
+                        <button className='login__button'>SIGN IN</button>
+                        <p className='login__forgotPassword'>FORGOT PASSWORD?</p>
 
-                </form>
-                <p>or sign in with...</p>
-            </div >
+                    </form>
+                    <p>or sign in with...</p>
+                </div >
+            </div>
         </div>
     )
 }
