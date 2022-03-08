f7aa351d948786b05d2032c45d8a58365a03f888
ME <cheung.samuelyc@gmail.com>
2022-03-08 20:29:44 +0800

    adjusted styling for login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -18,22 +18,22 @@ const LoginPage = () => {
                     <a className='login__links-login'>LOGIN</a>
                     <a className='login__links-signUp'>SIGN IN</a>
                 </div>
-                <div>
 
-                    <form className='login__form'>
-                        <div className='login__email'>
-                            <p className='login__email-header'>EMAIL ADDRESS:</p>
-                            <input type='text' className='login__email-input' />
-                        </div>
-                        <div className='login__password'>
-                            <p className='login__password-header'>PASSWORD:</p>
-                            <input type='password' className='login__password-input' />
-                        </div>
-                        <button className='login__button'>SIGN IN</button>
-                        <p className='login__forgotPassword'>FORGOT PASSWORD?</p>
 
-                    </form>
-                </div >
+                <form className='login__form'>
+                    <div className='login__email'>
+                        <p className='login__email-header'>EMAIL ADDRESS:</p>
+                        <input type='text' className='login__email-input' />
+                    </div>
+                    <div className='login__password'>
+                        <p className='login__password-header'>PASSWORD:</p>
+                        <input type='password' className='login__password-input' />
+                    </div>
+                    <button className='login__button'>SIGN IN</button>
+                    <p className='login__forgotPassword'>FORGOT PASSWORD?</p>
+
+                </form>
+
             </div>
         </div>
     )
