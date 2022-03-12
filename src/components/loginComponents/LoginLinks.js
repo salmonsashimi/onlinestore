7d50e6ad4a06d6098fa74a9889a16617fc721690
ME <cheung.samuelyc@gmail.com>
2022-03-13 03:02:11 +0800

    styled loginLinks

--- src/components/loginComponents/LoginLinks.js
+++ src/components/loginComponents/LoginLinks.js
@@ -4,9 +4,13 @@ const LoginLinks = (props) => {
     console.log(props)
     return (
         <div className='loginLinks'>
-            <Link href='/register'><p className={`loginLinks__signUp ${props.page === 'register' ? 'loginLinks__active' : 'loginLinks__hover'}`}>JOIN</p></Link>
-            <Link href='/login'><p className={`loginLinks__signUp ${props.page === 'login' ? 'loginLinks__active' : 'loginLinks__hover'}`}>LOGIN</p></Link>
-        </div>
+            <div className={`loginLinks__container ${props.page === 'register' && 'loginLinks__active'}`}>
+                <Link href='/register'><p className='loginLinks__signUp'>JOIN</p></Link>
+            </div>
+            <div className={`loginLinks__container ${props.page === 'login' && 'loginLinks__active'}`}>
+                <Link href='/login'><p className='loginLinks__login'>LOGIN</p></Link>
+            </div>
+        </div >
     )
 }
 
