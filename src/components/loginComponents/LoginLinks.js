0eb725e876d345b3852736a116da6fb190d6ff32
ME <cheung.samuelyc@gmail.com>
2022-03-12 19:00:39 +0800

    added hover class when not on active page

--- src/components/loginComponents/LoginLinks.js
+++ src/components/loginComponents/LoginLinks.js
@@ -4,8 +4,8 @@ const LoginLinks = (props) => {
     console.log(props)
     return (
         <div className='loginLinks'>
-            <Link href='/register'><p className={`loginLinks__signUp ${props.page === 'register' && 'loginLinks__active'}`}> SIGN UP</p></Link>
-            <Link href='/login'><p className={`loginLinks__signUp ${props.page === 'login' && 'loginLinks__active'}`}>LOGIN</p></Link>
+            <Link href='/register'><p className={`loginLinks__signUp ${props.page === 'register' ? 'loginLinks__active' : 'loginLinks__hover'}`}>JOIN</p></Link>
+            <Link href='/login'><p className={`loginLinks__signUp ${props.page === 'login' ? 'loginLinks__active' : 'loginLinks__hover'}`}>LOGIN</p></Link>
         </div>
     )
 }
