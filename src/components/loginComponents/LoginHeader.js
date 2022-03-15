48f1cf84597ffd5307c83b4690c59f99c87cfeb5
ME <cheung.samuelyc@gmail.com>
2022-03-15 16:06:53 +0800

    added link back to index page

--- src/components/loginComponents/LoginHeader.js
+++ src/components/loginComponents/LoginHeader.js
@@ -1,8 +1,10 @@
+import Link from 'next/link';
+
 const LoginHeader = () => (
     <div className='login__header'>
         <h1 className='login__header-text'>WELCOME TO</h1>
         <div className='login__header-logo'>
-            <h1 className='login__header-logo-text'>NAVY</h1>
+            <Link href='/'><h1 className='login__header-logo-text'>NAVY</h1></Link>
         </div>
     </div>
 )
