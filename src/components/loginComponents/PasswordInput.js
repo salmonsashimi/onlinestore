58294f80e673f0af4644d7af04b90425a4e9d394
ME <cheung.samuelyc@gmail.com>
2022-03-21 01:17:37 +0800

    setup hooks for form input

--- src/components/loginComponents/PasswordInput.js
+++ src/components/loginComponents/PasswordInput.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
-const PasswordInput = () => {
-    const [password, setPassword] = useState('')
+const PasswordInput = (props) => {
+    const { password, setPassword } = props;
     const [showPassword, setShowPassword] = useState(true)
 
     const onPasswordButtonClick = (e) => {
@@ -9,11 +9,21 @@ const PasswordInput = () => {
         setShowPassword(!showPassword)
     }
 
-
     return (
         <div className='passwordInput'>
-            <input type={showPassword ? 'password' : 'text'} className='login__input-input passwordInput__input' onChange={(e) => setPassword(e.target.value)} value={password} />
-            <button onClick={onPasswordButtonClick} className={`passwordInput__button ${password && 'passwordInput__button-show'}`}>{showPassword ? 'SHOW' : 'HIDE'}</button>
+            <input
+                type={showPassword ? 'password' : 'text'}
+                className='login__input-input passwordInput__input'
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+            />
+            <button
+                name='password'
+                onClick={onPasswordButtonClick}
+                className={`passwordInput__button ${password && 'passwordInput__button-show'}`}
+            >
+                {showPassword ? 'SHOW' : 'HIDE'}
+            </button>
         </div>
     )
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import LoginHeader from '../components/loginComponents/LoginHeader';
 import LoginLinks from '../components/loginComponents/LoginLinks';
 import PasswordInput from '../components/loginComponents/PasswordInput';
 
 const LoginPage = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        console.log('email: ', email)
+        console.log('password: ', password)
+    }
+
+
     return (
         <div className='login'>
             <Head>
@@ -12,18 +24,17 @@ const LoginPage = () => {
             <LoginHeader />
             <div className='login__component'>
                 <LoginLinks page='login' />
-                <form className='login__form'>
+                <form className='login__form' onSubmit={handleLogin}>
                     <div className='login__input'>
                         <p className='login__input-header'>EMAIL ADDRESS:</p>
-                        <input type='text' className='login__input-input' />
+                        <input name='email' type='text' className='login__input-input' value={email} onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div className='login__input'>
                         <p className='login__input-header'>PASSWORD:</p>
-                        <PasswordInput />
+                        <PasswordInput password={password} setPassword={setPassword} />
                     </div>
-                    <button className='login__button'>SIGN IN</button>
+                    <button type='submit' className='login__button'>SIGN IN</button>
                     <p className='login__forgotPassword'>FORGOT PASSWORD?</p>
-
                 </form>
 
             </div>
