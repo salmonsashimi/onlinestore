1ab9e8180d91eebe32e0db2b6f873a7d50c79fcd
ME <cheung.samuelyc@gmail.com>
2022-03-20 23:09:50 +0800

    added toggle button for password show/hide

--- src/components/loginComponents/PasswordInput.js
+++ src/components/loginComponents/PasswordInput.js
@@ -2,11 +2,18 @@ import { useState } from 'react';
 
 const PasswordInput = () => {
     const [password, setPassword] = useState('')
+    const [showPassword, setShowPassword] = useState(true)
+
+    const onPasswordButtonClick = (e) => {
+        e.preventDefault();
+        setShowPassword(!showPassword)
+    }
+
 
     return (
         <div className='passwordInput'>
-            <input type='password' className='login__input-input passwordInput__input' onChange={(e) => setPassword(e.target.value)} value={password} />
-            <button className={`passwordInput__button ${password && 'passwordInput__button-show'}`}>SHOW</button>
+            <input type={showPassword ? 'password' : 'text'} className='login__input-input passwordInput__input' onChange={(e) => setPassword(e.target.value)} value={password} />
+            <button onClick={onPasswordButtonClick} className={`passwordInput__button ${password && 'passwordInput__button-show'}`}>{showPassword ? 'SHOW' : 'HIDE'}</button>
         </div>
     )
 }
