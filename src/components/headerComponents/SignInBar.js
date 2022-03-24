804e527b2efba600a2890e03b3e86de8051a9a7e
ME <cheung.samuelyc@gmail.com>
2022-03-24 22:51:06 +0800

    added conditional for sign in bar

--- src/components/headerComponents/SignInBar.js
+++ src/components/headerComponents/SignInBar.js
@@ -1,19 +1,34 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/CartContext';
 import Link from 'next/link'
-const SignInBar = () => (
-    <div className='signInBar'>
-        <div className='container signInBar__content'>
-            <div className='signInBar__left'>
-                <p className='signInBar__link'>FACEBOOK</p>
-                <p className='signInBar__link'>INSTAGRAM</p>
-                <p className='signInBar__link'>YOUTUBE</p>
-            </div>
-            <div className='signInBar__right'>
-                <p className='signInBar__link'>TRACK MY ORDER</p>
-                <Link href='/register'><p className='signInBar__link'>SIGN UP</p></Link>
-                <Link href='/login'><p className='signInBar__link'>LOG IN</p></Link>
+
+const SignInBar = () => {
+    const context = useContext(CartContext);
+    const { token } = context;
+    console.log(token)
+
+    return (
+        <div className='signInBar'>
+            <div className='container signInBar__content'>
+                <div className='signInBar__left'>
+                    <p className='signInBar__link'>FACEBOOK</p>
+                    <p className='signInBar__link'>INSTAGRAM</p>
+                    <p className='signInBar__link'>YOUTUBE</p>
+                </div>
+                <div className='signInBar__right'>
+                    <p className='signInBar__link'>TRACK MY ORDER</p>
+
+                    {token.length === 0 ?
+                        <>
+                            <Link href='/register'><p className='signInBar__link'>SIGN UP</p></Link>
+                            <Link href='/login'><p className='signInBar__link'>LOG IN</p></Link>
+                        </>
+                        :
+                        <Link href='/user'><p className='signInBar__link'>USER PAGE</p></Link>}
+                </div>
             </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default SignInBar;
