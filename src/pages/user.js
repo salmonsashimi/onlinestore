46fdf674ed247c86540b4bd3d5ad4f8fdd0a43de
ME <cheung.samuelyc@gmail.com>
2022-03-27 01:06:53 +0800

    initialise layout for user page

--- src/pages/user.js
+++ src/pages/user.js
@@ -1,7 +1,17 @@
+import Layout from '../components/Layout';
+
 const UserPage = () => (
     <div>
         user page
     </div>
 )
+
+UserPage.getLayout = function getLayout(page) {
+    return (
+        <Layout>
+            {page}
+        </Layout>
+    )
+}
 
 export default UserPage;
