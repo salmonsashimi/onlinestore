75911e228da889a5045b70ad954bf6c9843c2240
ME <cheung.samuelyc@gmail.com>
2022-04-03 20:27:54 +0800

    changed error messages for login api

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -11,19 +11,17 @@ export default async function (req, res) {
         // const email = "bennytan@example.com"
         // const password = 'n3920nf0q9n230fnq20n9';
         const user = await col.findOne({ "email": email })
-        console.log('user', user);
 
         //check if user exists
         if (!user) {
-            console.log('null user')
-            return res.status(200).json({ error: 'User not found' })
+            return res.status(500).json({ error: 'User does not exist.' })
         }
 
         //verify user password
         if (user.password === password) {
             return res.send({ token: 'test123' })
         } else {
-            return res.status(500).json({ error: 'Wrong password' })
+            return res.status(500).json({ error: 'Incorrect password, please try again.' })
         }
     }
 }
