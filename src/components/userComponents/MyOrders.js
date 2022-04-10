f2224a56501e17c85f87ba5d7241e075baee013b
ME <cheung.samuelyc@gmail.com>
2022-04-10 16:07:43 +0800

    styled MyOrders component

--- src/components/userComponents/MyOrders.js
+++ src/components/userComponents/MyOrders.js
@@ -1,14 +1,17 @@
 const MyOrders = () => (
     <div className='myOrders'>
-        <h1>DELIVERED!</h1>
-        <p>ORDER DATE 10 APRIL 2022</p>
-        <p>ORDER NUMBER</p>
-        <p>ITEMS HERE</p>
-        <div className='myOrders__buttons'>
-            <button>TRACK PARCEL</button>
-            <button>VIEW ORDER</button>
+        <div className='myOrders__order'>
+            <div className='myOrders__order-text'>
+                <h3 className='myOrders__order-header'>IT'S DELIVERED!</h3>
+                <p>ORDER DATE: 10 APRIL 2022</p>
+                <p>ORDER NUMBER: 12312341234</p>
+                <p>ITEMS HERE</p>
+            </div>
+            <div className='myOrders__order-buttons'>
+                <button className='myOrders__order-button'>TRACK PARCEL</button>
+                <button className='myOrders__order-button'>VIEW ORDER</button>
+            </div>
         </div>
-
     </div>
 )
 
