46db6bb16a268372628c2a18a44785df3e57f738
ME <cheung.samuelyc@gmail.com>
2022-04-15 23:24:40 +0800

    added name to anonymous arrow functions to preserve local state

--- src/components/indexComponents/IndexCateogry.js
+++ src/components/indexComponents/IndexCateogry.js
@@ -1,6 +1,8 @@
-export default (props) => (
+const IndexCategory = (props) => (
     <div className='indexCategory'>
         <img className='indexCategory__image' src={props.category.img} />
         <h1 className='indexCategory__name'>{props.category.name}</h1>
     </div>
 )
+
+export default IndexCategory;
--- src/components/indexComponents/Promo.js
+++ src/components/indexComponents/Promo.js
@@ -1,7 +1,9 @@
-export default (props) => (
+const Promo = (props) => (
     <div className='promo'>
         <img src={props.promo.imgUrl}></img>
         <h3 className='promo__title'>{props.promo.name}</h3>
         <p className='promo__text'>{props.promo.text}</p>
     </div>
 )
+
+export default Promo;
