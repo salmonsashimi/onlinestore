b389dc2362589f6fb5cacb6cdc4ea348f3e35ab3
ME <cheung.samuelyc@gmail.com>
2022-04-25 00:41:38 +0800

    added api fetch to retrieve user data

--- src/components/userComponents/AccountOverview.js
+++ src/components/userComponents/AccountOverview.js
@@ -1,17 +1,29 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { CartContext } from '../../contexts/CartContext';
 
 const AccountOverview = () => {
     const context = useContext(CartContext);
     const { token } = context;
 
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('');
 
+    const retrieveUserInfo = async (id) => {
+        const userInfoStr = await fetch(`http://localhost:3000/api/user/${id}`);
+        const userInfo = await userInfoStr.json();
+        const { name, email } = userInfo;
+        setName(name);
+        setEmail(email);
 
+    }
+    useEffect(() => {
+        retrieveUserInfo(token)
+    })
 
     return (
         <div className='accountOverview'>
-            <p>name</p>
-            <p>email</p>
+            <p>{name}</p>
+            <p>{email}</p>
             <p>address</p>
         </div>
     )
