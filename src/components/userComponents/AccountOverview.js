4f1d7f02271bf8c921f03a348865e421835ba758
ME <cheung.samuelyc@gmail.com>
2022-04-25 00:37:25 +0800

    retrieve token for AccountOverview

--- src/components/userComponents/AccountOverview.js
+++ src/components/userComponents/AccountOverview.js
@@ -1,9 +1,20 @@
-const AccountOverview = () => (
-    <div className='accountOverview'>
-        <p>name</p>
-        <p>email</p>
-        <p>address</p>
-    </div>
-)
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/CartContext';
+
+const AccountOverview = () => {
+    const context = useContext(CartContext);
+    const { token } = context;
+
+
+
+
+    return (
+        <div className='accountOverview'>
+            <p>name</p>
+            <p>email</p>
+            <p>address</p>
+        </div>
+    )
+}
 
 export default AccountOverview;
