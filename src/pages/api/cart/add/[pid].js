1f6a6e4fce9c1dd72bdb2b8166d9204fbc85e5ce
ME <cheung.samuelyc@gmail.com>
2022-04-29 00:09:26 +0800

    set structure for cart retrieval

--- src/pages/api/cart/add/[pid].js
+++ src/pages/api/cart/add/[pid].js
@@ -1,1 +1,15 @@
-console.log('hi')
+import clientPromise from '../../../../lib/mongodb';
+
+export default async (req, res) => {
+    const client = await clientPromise;
+    const db = client.db('online-store');
+    const col = db.collection('cart');
+
+
+    const cartItem = {
+        userId,
+        cartItems
+    }
+
+    const currentCart = req.body;
+}
