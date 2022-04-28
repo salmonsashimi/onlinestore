2d77ce6b85dfd11cd9dd35df574fe51c6cf0f133
ME <cheung.samuelyc@gmail.com>
2022-04-28 18:16:18 +0800

    removed cart schema

--- src/pages/api/register.js
+++ src/pages/api/register.js
@@ -13,7 +13,6 @@ export default async function (req, res) {
             email,
             password,
             contactPref,
-            cart: {}
         }
         console.log(userDocument)
 
