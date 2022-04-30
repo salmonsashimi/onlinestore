7f7fa624624b354d5fb0c045c18092fb1c96fbb3
ME <cheung.samuelyc@gmail.com>
2022-04-30 10:55:06 +0800

    added updateCart api

--- src/pages/api/cart/updateCart.js
+++ src/pages/api/cart/updateCart.js
@@ -1,10 +1,9 @@
-import clientPromise from '../../../../lib/mongodb';
+import clientPromise from '../../../lib/mongodb';
 
 export default async (req, res) => {
     const client = await clientPromise;
     const db = client.db('online-store');
     const col = db.collection('cart');
-
 
     // const cartItem = {
     //     userId,
@@ -13,4 +12,15 @@ export default async (req, res) => {
 
     const currentCart = req.body;
     console.log(currentCart)
+
+
+    //check if previous cart exists
+    const prevCart = await col.findOne({ _id: 1 });
+
+    if (prevCart) {
+        console.log('previous cart exists', prevCart)
+    } else {
+        console.log('previous cart does not exist')
+    }
+
 }
