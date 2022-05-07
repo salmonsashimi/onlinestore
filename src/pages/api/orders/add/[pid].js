9f64e5a406ab11ed3df98a41671ed774845c4061
ME <cheung.samuelyc@gmail.com>
2022-05-07 23:14:21 +0800

    created add api for orders

---
+++ src/pages/api/orders/add/[pid].js
@@ -0,0 +1,11 @@
+import clientPromise from '../../../../lib/mongodb';
+
+export default async function (req, res) {
+    const client = await clientPromise;
+    const db = client.db('online-store');
+    const col = db.collection('orders');
+
+    const { pid } = req.query
+    console.log(pid);
+
+}
