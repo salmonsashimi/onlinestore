ff8b0d77c4c51ff060d4f5d33ae905ee2d0fa9cf
ME <cheung.samuelyc@gmail.com>
2022-05-08 01:03:39 +0800

    linked add order api to mongo

--- src/pages/api/orders/add.js
+++ src/pages/api/orders/add.js
@@ -11,4 +11,18 @@ export default async function (req, res) {
     console.log(token)
     console.log(cart);
 
+    // Construct a document                                                                                                                                                              
+    let orderDocument = {
+        "userId": token,
+        "orders": cart
+    }
+
+    console.log('orderdocument', orderDocument);
+
+    // Insert a single document, wait for promise so we can read it back
+    const p = await col.insertOne(orderDocument);
+    // Find one document
+    const myDoc = await col.find();
+    // Print to the console
+
 }
