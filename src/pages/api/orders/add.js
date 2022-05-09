3c7dc4ccf9b1ce18d22fd171b0c9cfe39e727223
ME <cheung.samuelyc@gmail.com>
2022-05-09 21:15:19 +0800

    cleanup add order api

--- src/pages/api/orders/add.js
+++ src/pages/api/orders/add.js
@@ -5,19 +5,13 @@ export default async function (req, res) {
     const db = client.db('online-store');
     const col = db.collection('orders');
 
-    console.log('add page')
-
     const { token, cart } = req.body
-    console.log(token)
-    console.log(cart);
 
     // Construct a document                                                                                                                                                              
     let orderDocument = {
         "userId": token,
-        "orders": cart
+        "items": cart
     }
-
-    console.log('orderdocument', orderDocument);
 
     // Insert a single document, wait for promise so we can read it back
     const p = await col.insertOne(orderDocument);
