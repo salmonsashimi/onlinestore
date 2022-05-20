870f7bc39ec27d6324b8c5e95e5a91d471a77cdd
ME <cheung.samuelyc@gmail.com>
2022-05-20 10:34:34 +0800

    added details from order api

--- src/pages/order/[pid].js
+++ src/pages/order/[pid].js
@@ -1,7 +1,7 @@
 import Layout from '../../components/Layout';
 
 const CheckoutPage = ({ data }) => {
-    const { _id, items, totalPrice, deliveryMethod } = data;
+    const { _id, items, totalPrice, delivery } = data;
     console.log(data)
     console.log(_id)
 
@@ -10,13 +10,15 @@ const CheckoutPage = ({ data }) => {
             <h1 className='order__header'>ORDER SUMMARY</h1>
             <div className='order__components'>
                 <div className='order__left'>
-                    <p>ORDER NUMBER: {_id}</p>
-                    <h1>items</h1>
+                    <p>ORDER REFERENCE: {_id}</p>
+                    <h1>ORDER ITEMS</h1>
+                    {items.map(item => <p>{item.name}</p>)}
+
                 </div>
                 <div className='right'>
-                    <h1>subtotal: ${totalPrice}</h1>
-                    <h1>shipping</h1>
-                    <h1>total cost</h1>
+                    <h1>SUBTOTAL: ${totalPrice}</h1>
+                    <h1>SHIPPING: {delivery}</h1>
+                    <h1>TOTAL: ${totalPrice}</h1>
                 </div>
             </div>
             <button>Return to Main</button>
